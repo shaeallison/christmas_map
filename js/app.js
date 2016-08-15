dd75b790b743db7024dabe53932c366e2929da40
Shae Allison <shaeallison@Shaes-MacBook-Pro.local>
2016-08-15 10:31:52 -0400

    ng-show working with each city

--- js/app.js
+++ js/app.js
@@ -2,22 +2,8 @@ var app = angular.module('myApp', ['cities', 'angularModalService']);
 
 app.controller('BaseController', ['$scope', 'ModalService', function($scope, ModalService) {
 
-  $scope.details = {show: false}; /* only piece needed for ng-show */
+ /*  $scope.details = {show: true}; */ /* only piece needed for ng-show */
 
-  /* $scope.customResult = null;
-
-  $scope.showCustom = function() {
-
-    ModalService.showModal({
-      templateUrl: "custom-modal.html",
-      controller: "CustomController"
-    }).then(function(modal) {
-      modal.close.then(function(result) {
-        $scope.customResult = "All good!";
-      });
-    });
-
-  }; */
 
 }]);
 
